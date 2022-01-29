8a772eca8e859a4ad314158553054df5dc063daf
渡部太貴 <watanabefutoshitaka@watanabefutoshitakanoMacBook-Pro.local>
2022-01-29 18:08:33 +0900

    20220129 nabe

--- src/screens/SC998_Nabe/SC998_V00_Nabe.tsx
+++ src/screens/SC998_Nabe/SC998_V00_Nabe.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 import { Button, Modal, Center, NativeBaseProvider } from "native-base"
 import {
+    Button as SimpleBtn,
     StyleSheet,
     TextInput,
     SafeAreaView,
@@ -12,12 +13,23 @@ import {
     Dimensions
 } from 'react-native';
 import { CC0010_ScreenTitle } from '../SC000_BaseComponent/SC000_V02_ScreenTitle'
+import { SC998_V01_cmp1  }  from './SC998_V01_cmp1 '
+import { SC998_V02_cmp2 } from './SC998_V02_cmp2'
+import { SC998_V03_cmp3 } from './SC998_V03_cmp3'
+
+
 
 export const SC998_V00_Nabe = () => {
     return (
         <SafeAreaView>
             <CC0010_ScreenTitle >なべ画面</CC0010_ScreenTitle>
             <Text>むらたはじめ</Text>
+            <Text>う</Text>
+            <SC998_V01_cmp1/>
+            <SC998_V02_cmp2/>
+            <SC998_V03_cmp3/>
+            
+
         </SafeAreaView>
     )
 }
---
+++ src/screens/SC998_Nabe/SC998_V02_cmp2.tsx
@@ -0,0 +1,32 @@
+import React from "react"
+import { Button, Modal, Center, NativeBaseProvider } from "native-base"
+import {
+    Button as SimpleBtn,
+    StyleSheet,
+    TextInput,
+    SafeAreaView,
+    KeyboardAvoidingView,
+    View,
+    Text,
+    FlatList,
+    Alert,
+    Dimensions
+} from 'react-native';
+import { CC0010_ScreenTitle } from '../SC000_BaseComponent/SC000_V02_ScreenTitle'
+import { SC998_Style } from './SC998_Style'
+
+const log = () =>{
+    console.log("あ")
+}
+
+export const SC998_V02_cmp2 = () => {
+    return (
+        <>
+            <Text>こ</Text>
+            <View style={SC998_Style.v00_nabebts}>
+                <SimpleBtn title="ぼたん" onPress={log} />  
+
+            </View>
+        </>
+    )
+}
