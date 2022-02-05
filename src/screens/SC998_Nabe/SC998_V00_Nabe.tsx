24f38cd0a626bb495d91607a87fc8a49b90839ab
tnGithub069 <g2.pokest.0777@gmail.com>
2022-02-05 15:16:48 +0900

    20220205 tsune

--- src/screens/SC998_Nabe/SC998_V00_Nabe.tsx
+++ src/screens/SC998_Nabe/SC998_V00_Nabe.tsx
@@ -13,24 +13,24 @@ import {
     Dimensions
 } from 'react-native';
 import { CC0010_ScreenTitle } from '../SC000_BaseComponent/SC000_V02_ScreenTitle'
-import { SC998_V01_Cmp1  }  from './SC998_V01_Cmp1 '
+import { SC998_V01_Cmp1 } from './SC998_V01_Cmp1 '
 import { SC998_V02_Cmp2 } from './SC998_V02_Cmp2'
 import { SC998_V03_Cmp3 } from './SC998_V03_Cmp3'
-import {SC998_S_Provider} from './SC998_Store'
+import { SC998_S_Provider } from './SC998_Store'
 
 
 export const SC998_V00_Nabe = () => {
- 
+
     return (
         <SafeAreaView>
             <CC0010_ScreenTitle >なべ画面</CC0010_ScreenTitle>
             <Text>むらたはじめ</Text>
 
-         <SC998_S_Provider>
-            <SC998_V01_Cmp1/>
-            <SC998_V02_Cmp2/>
-            <SC998_V03_Cmp3/>
-        </SC998_S_Provider>
+            <SC998_S_Provider>
+                <SC998_V01_Cmp1 />
+                <SC998_V02_Cmp2 />
+                <SC998_V03_Cmp3 />
+            </SC998_S_Provider>
         </SafeAreaView>
     )
 }
